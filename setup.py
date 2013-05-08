9f6ce66d935eaa8da8c810a50e1344895fbd94b5
brainpower <brainpower@gulli.com>
2013-05-08 20:56:37 +0200

    * initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='bpacman',
+      version='0.1.0',
+			description="A pacman GUI written in python using pyalpm.",
+			author="brainpower",
+			author_email="brainpower@gulli.com",
+			url="",
+			packages=['bpacman']
+)
