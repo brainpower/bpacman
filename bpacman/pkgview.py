34cd30b08404325019f1b126bfccaea5c6e6b9c4
brainpower <brainpower@gulli.com>
2013-05-09 17:13:11 +0200

    * fixed sizes and margin

--- bpacman/pkgview.py
+++ bpacman/pkgview.py
@@ -6,14 +6,15 @@ from bpacman.pkglistanddesc import PkgListAndDesc
 class PkgView(Gtk.Paned):
 
 	def __init__(self, *args):
-		Gtk.Paned.__init__(self, *args);
+		Gtk.Paned.__init__(self, margin=4, *args);
 
 		self.set_orientation(Gtk.Orientation.HORIZONTAL);
 		self.set_vexpand(True)
 
-		tlst = Gtk.ListStore(str)
-		sidebar = Sidebar();
-		pkglist = PkgListAndDesc(tlst, orientation=Gtk.Orientation.VERTICAL);
+		pkglist = PkgListAndDesc(orientation=Gtk.Orientation.VERTICAL);
+		sidebar = Sidebar(pkglist);
+
+		sidebar.set_property("width-request", 180)
 
 		self.add(sidebar);
 		self.add(pkglist);
