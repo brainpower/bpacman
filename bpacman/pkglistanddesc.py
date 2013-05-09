b0b68e9766785e1250e47c907a67427726b76752
brainpower <brainpower@gulli.com>
2013-05-09 17:12:08 +0200

    * PkgListAndDesc implemented

--- bpacman/pkglistanddesc.py
+++ bpacman/pkglistanddesc.py
@@ -1,31 +1,100 @@
-
+import sys
 from gi.repository import Gtk, Pango
 from bpacman.pacman import Pacman
 
 class PkgListAndDesc(Gtk.Paned):
 
-	def __init__(self, model, *args, **kwargs):
+	def __init__(self, *args, **kwargs):
 		Gtk.Paned.__init__(self, *args, **kwargs);
 
-		self.tbuff = Gtk.TextBuffer();
-		self.create_default_tags();
+		self._tbuff = Gtk.TextBuffer();
+		self._create_default_tags();
+		self._update_description("bpacman", "A graphical frontend to pacman, the Arch Linux package manager, inspired by the synaptic package manager.")
 
-		begin = self.tbuff.get_start_iter()
+		sw1 = Gtk.ScrolledWindow(hexpand=True)
+		sw2 = Gtk.ScrolledWindow(hexpand=True)
 
-		self.tbuff.insert_with_tags_by_name(begin, "Description\n", "h1");
-		self.tbuff.insert(begin, "\nSome blah, blah...\n")
-		self.tbuff.insert(begin, "Some blah, blah...")
+		self._models = {}
+		self._sort_type = Gtk.SortType.ASCENDING;
 
-		sw1 = Gtk.ScrolledWindow(vexpand=True)
-		sw2 = Gtk.ScrolledWindow(vexpand=True)
+		self._tv = Gtk.TreeView();
+		self._tv.set_headers_clickable(True)
+		self._create_columns();
+		descr = Gtk.TextView(buffer=self._tbuff, editable=False, cursor_visible=False, left_margin=8, right_margin=8);
+		descr.set_wrap_mode(Gtk.WrapMode.WORD)
 
-		treeview = Gtk.TreeView(model);
-		descr = Gtk.TextView(buffer=self.tbuff, editable=False, cursor_visible=False, left_margin=8, right_margin=8);
+		self._tv.get_selection().connect("changed", self._tv_selection_changed)
 
-		sw1.add(treeview);
+		sw1.add(self._tv);
 		sw2.add(descr);
-		self.add(sw1);
-		self.add(sw2);
+		sw1.set_property("height-request", 130)
 
-	def create_default_tags(self):
-		self.tbuff.create_tag( "h1", size_points=16, weight=Pango.Weight.BOLD, pixels_above_lines=8)
+		self.pack1(sw1, resize=True);
+		self.pack2(sw2);
+
+	def _create_default_tags(self):
+		self._tbuff.create_tag( "h1", size_points=16, weight=Pango.Weight.BOLD, pixels_above_lines=8)
+		self._tbuff.create_tag( "b", weight=Pango.Weight.BOLD)
+
+	def _create_columns(self):
+		state = Gtk.TreeViewColumn(None, Gtk.CellRendererToggle(), active=1)
+		name  = Gtk.TreeViewColumn("Name", Gtk.CellRendererText(), text=2)
+		iver  = Gtk.TreeViewColumn("Installed Version", Gtk.CellRendererText(), text=3)
+		lver  = Gtk.TreeViewColumn("Latest Version", Gtk.CellRendererText(), text=4)
+		desc  = Gtk.TreeViewColumn("Description", Gtk.CellRendererText(), text=5)
+		state.set_resizable(True)
+		name.set_resizable(True)
+		iver.set_resizable(True)
+		lver.set_resizable(True)
+		desc.set_resizable(True)
+		name.set_clickable(True)
+		name.connect("clicked", self._swap_sort_order)
+		self._tv.append_column(state)
+		self._tv.append_column(name)
+		self._tv.append_column(iver)
+		self._tv.append_column(lver)
+		self._tv.append_column(desc)
+
+	def _swap_sort_order(self, b):
+		print("swap")
+		if self._sort_type == Gtk.SortType.ASCENDING:
+			self._sort_type = Gtk.SortType.DESCENDING
+		else:
+			self._sort_type = Gtk.SortType.ASCENDING;
+		self._tv.get_model().set_sort_column_id(2, self._sort_type)
+
+	def _tv_selection_changed(self, selection):
+		pacman = Pacman.Instance();
+		if not selection:
+			return
+		model,it = selection.get_selected()
+		if model:
+			repo = self._tv.get_model()[it][0]
+			pkgn = model[it][2]
+			pkg = pacman.get_package(repo, pkgn);
+			self._update_description(pkgn, pkg.desc)
+
+	def _update_description(self, pkgname, desc):
+		self._tbuff.set_text("")
+		begin = self._tbuff.get_start_iter()
+
+		self._tbuff.insert_with_tags_by_name(begin, pkgname+"\n", "h1");
+		self._tbuff.insert(begin, "\n")
+		self._tbuff.insert(begin, desc)
+
+	#~ def _update_models(self):
+		#~ pacman = Pacman.Instance();
+#~
+		#~ self._models["All"] = Gtk.ListStore(str)
+		#~ for repo in pacman.get_repos():
+			#~ for pkg in pacman.get_package_list(repo):
+				#~ self._models["All"].append([pkg.name])
+
+	def add_pkg_model(self, name, model):
+		self._models[name] = model;
+
+	def set_package_model(self, model):
+		try:
+			self._tv.set_model(self._models[model])
+		except KeyError:
+			sys.stderr.write("[PkgListAndDesc] Warning: Model for '%s' not registered.\n" % model)
