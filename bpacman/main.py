35e8dd9fca8b3e24c4407ef54894a43874b868a3
brainpower <brainpower@gulli.com>
2013-05-09 02:30:36 +0200

    * implemented main properly

--- bpacman/main.py
+++ bpacman/main.py
@@ -1,3 +1,9 @@
 
+from gi.repository import Gtk
+from bpacman.main_window import MainWindow
+
 def main():
-	pass
+	win = MainWindow("bpacman 0.1.0");
+	win.connect("delete-event", Gtk.main_quit);
+	win.show_all();
+	Gtk.main();
