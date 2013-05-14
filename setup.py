474fb37e81aabfcaa2caa15549805b0e04b00f77
brainpower <brainpower@gulli.com>
2013-05-14 19:34:23 +0200

    * added data_files for state icons

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 from distutils.core import setup
+from distutils import sysconfig
+import sys
+
 
 setup(name='bpacman',
       version='0.1.0',
@@ -9,5 +12,21 @@ setup(name='bpacman',
 			author_email="brainpower@gulli.com",
 			url="",
 			packages=['bpacman'],
-			scripts=['src/bpacman']
+			scripts=['src/bpacman'],
+			data_files = [('share/bpacman', ['data/package-available.png',
+			                                 'data/package-install.png',
+			                                 'data/package-remove.png',
+			                                 'data/package-installed-outdated.png',
+			                                 'data/package-installed-updated.png',
+			                                 'data/package-reinstall.png',
+			                                 'data/package-available.png',
+			                                # 'data/',
+			                                 'data/package-upgrade.png',])]
 )
+
+#package-available-locked.png
+#package-broken.png
+#package-downgrade.png
+#package-purge.png
+#package-new.png
+#package-installed-locked.png
