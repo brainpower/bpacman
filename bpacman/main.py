e1448878267bbd045ad16e2f26924b76fe1aafdf
brainpower <brainpower@gulli.com>
2013-05-14 19:25:41 +0200

    * make gtk threading aware

--- bpacman/main.py
+++ bpacman/main.py
@@ -1,9 +1,14 @@
 
-from gi.repository import Gtk
-from bpacman.main_window import MainWindow
+from gi.repository import Gtk,Gdk,GLib
+from bpacman.main_window import *
 
 def main():
+	print("bpacman 0.1.0 \"What a Pain\" using libalpm", pyalpm.alpmversion())
 	win = MainWindow("bpacman 0.1.0");
 	win.connect("delete-event", Gtk.main_quit);
 	win.show_all();
-	Gtk.main();
+	GLib.threads_init()
+	Gdk.threads_init()
+	Gdk.threads_enter()
+	Gtk.main()
+	Gdk.threads_leave()
